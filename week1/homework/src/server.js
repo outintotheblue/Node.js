dcdfd585cb3081d3041eac0070d96590792ebea1
unknown <martinaoyhenard@gmail.com>
2019-09-03 23:14:33 +0200

    5 tests failed

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,12 +9,33 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+
+    switch (request.url) {
+      case '/state':
+        response.write(JSON.stringify({ state: state }));
+        break;
+      case '/add':
+        state += 1;
+        response.write(JSON.stringify({ state: state }));
+        break;
+      case '/substract':
+        state -= 1;
+        response.write(JSON.stringify({ state: state }));
+        break;
+      case '/reset':
+        response.write(JSON.stringify({ state: state }));
+        break;
+      default:
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.write(JSON.stringify({ error: 'Not found' }));
+    }
+    response.end();
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
