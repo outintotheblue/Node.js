5dfc7c26ecdce0ac33bc4620b2aac9fe5838f5ed
unknown <martinaoyhenard@gmail.com>
2019-09-06 13:55:35 +0200

    less code

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,29 +5,31 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
+
 function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
+    function writeLines() {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify({ state: state }));
+    }
+
     switch (request.url) {
       case '/state':
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ state: state }));
+        writeLines();
         break;
       case '/add':
         state += 1;
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ state: state }));
+        writeLines();
         break;
       case '/subtract':
         state -= 1;
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ state: state }));
+        writeLines();
         break;
       case '/reset':
         state = 10;
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ state: state }));
+        writeLines();
         break;
       default:
         response.writeHead(404, { 'Content-Type': 'application/json' });
