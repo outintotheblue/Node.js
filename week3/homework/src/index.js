c1d91d3dcf33fb383bbad353ba6e9f869d729362
unknown <martinaoyhenard@gmail.com>
2019-09-20 13:16:10 +0200

    not done, clear

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,15 +1,36 @@
 'use strict';
 
-// TODO: Write the homework code in this file
 function readTodoWithId() {
   const id = request.params.id;
-
-  app.get('/todo/:id', function(req, res) {
-    res.send('id: ' + request.params.id);
+	app.get('/todo/:id', function (req, res) {
+		const getToDo = getElementById(req.params.id, todo);
+		if (getToDo) {
+			res.send(getToDo);
+		} else {
+			res.status(404).send();
+		}
   });
 }
 
-function clearTodos() {}
+function clearTodos() {
+	app.delete('/todo', (req, res, next) => {
+	res.send(todo)
+	});
+}
 
+function markAsDone() {
+app.post('/todos/:id', (req, res, next) => {
+	}
+}
 
-function markAsDone()
+function markAsNotDone()  {
+	app.delete('/todo/:id', (req, res, next) => {
+		const todoIndex = getIndexById(req.params.id, todo);
+		if (todoIndex !== -1) {
+			todo.splice(todoIndex, 1);
+			res.status(204).send();
+		} else {
+			res.status(404).send();
+		}
+	});
+}
