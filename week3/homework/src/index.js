d4b12d1f3d863bbdd178a1efafaf129d7b49cd9a
unknown <martinaoyhenard@gmail.com>
2019-09-20 10:16:42 +0200

    read

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // TODO: Write the homework code in this file
+function readTodoWithId() {
+  const id = request.params.id;
+
+  app.get('/todo/:id', function(req, res) {
+    res.send('id: ' + request.params.id);
+  });
+}
+
+function clearTodos() {}
+
+
+function markAsDone()
