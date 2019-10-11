201273ec4a25d369438b0129dc172028dde3f6f7
unknown <martinaoyhenard@gmail.com>
2019-10-11 09:28:51 +0200

    changed endpoints

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -32,8 +32,8 @@ app.get(`/${TODO_SLUG}`,        readTodos.bind(null, todo));
 app.put(`/${TODO_SLUG}/:id`,    updateTodo.bind(null, todo));
 app.delete(`/${TODO_SLUG}/:id`, deleteTodo.bind(null, todo));
 app.get(`/${TODO_SLUG}/:id`,    readTodo.bind(null, todo));
-app.put(`/${TODO_SLUG}/:id/done`,    markDone.bind(null, todo));
-app.put(`/${TODO_SLUG}/:id/notdone`,    markAsNotDone.bind(null, todo));
+app.post(`/${TODO_SLUG}/:id/done`,    markDone.bind(null, todo));
+app.delete(`/${TODO_SLUG}/:id/done`,    markAsNotDone.bind(null, todo));
 app.delete(`/${TODO_SLUG}`, clearTodos.bind(null, todo));
 
 
